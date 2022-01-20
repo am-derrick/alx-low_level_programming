bc6501a06a5162faa4aad3322e20b438963f599b
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2022-01-20 09:52:05 +0300

    struct updated

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,10 +8,11 @@
  * struct listint_s - singly linked list
  *
  * @n: Integer
- * @index: index of node
- * @next: pointer to the next node
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct listint_s
 {
@@ -19,22 +20,6 @@ typedef struct listint_s
 	size_t index;
 	struct listint_s *next;
 } listint_t;
-
-/**
- * struct listint_s -singly linked list
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- *
- * Description: singly linked list node structure
- */
-typedef struct listint_s
-{
-	int n;
-	size_t index;
-	struct listint_s *next;
-} listint_t
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
